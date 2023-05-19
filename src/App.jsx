d4455618f09c54ea96f72f1e3f36964677b056f1
Felipe Soares <86936050+felipesoarws@users.noreply.github.com>
2023-05-19 09:28:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
 
     TagManager.initialize(tagManagerArgs);
     ReactGA.initialize('G-1ES9TLR117');
-    ReactGA.pageView(window.location.pathname);
+    ReactGA.pageview(window.location.pathname);
   }, []);
   return (
     <Router>
