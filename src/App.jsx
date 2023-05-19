82f009dc34e573cb48cf3e39e1af9a47c5efbd22
Felipe Soares <86936050+felipesoarws@users.noreply.github.com>
2023-05-18 22:25:09 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
-import Home from "./pages/Home.jsx";
-import Error from "./pages/Error.jsx";
-import Projects from "./pages/Projects.jsx";
+import Home from './pages/Home.jsx';
+import Error from './pages/Error.jsx';
+import Projects from './pages/Projects.jsx';
 
 export default function App() {
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmid: 'GTM-586T7P5',
+    };
+
+    TagManager.initialize(tagManagerArgs);
+  });
   return (
     <Router>
       <Routes>
