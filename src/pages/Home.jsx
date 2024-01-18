2e71ddcc0a555286d8476fd1cb0be8ba401f0e3a
Felipe Soares <86936050+felipesoarws@users.noreply.github.com>
2024-01-18 16:56:19 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -206,6 +206,7 @@ const Home = () => {
                   'JavaScript',
                   'ReactJS',
                   'ViteJS',
+                  'WordPress',
                 ]}
                 skillProgress={'competent'}
               />
